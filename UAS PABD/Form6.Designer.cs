8a106194e5e10226202730f8e28546e7a38b0861
panjialbir <115150326+panjialbir@users.noreply.github.com>
2023-07-08 20:51:48 +0700

    mengganti nama tool form 6

--- UAS PABD/Form6.Designer.cs
+++ UAS PABD/Form6.Designer.cs
@@ -48,7 +48,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(106, 39);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Open";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -57,7 +57,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 36);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Add";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -66,7 +66,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(105, 36);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Save";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -75,7 +75,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(105, 36);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "Clear";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // textBox1
@@ -111,36 +111,36 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(100, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(86, 20);
             this.label1.TabIndex = 8;
-            this.label1.Text = "label1";
+            this.label1.Text = "ID Cabang";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(100, 155);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.Size = new System.Drawing.Size(111, 20);
             this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
+            this.label2.Text = "Nama Cabang";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(100, 205);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.Size = new System.Drawing.Size(59, 20);
             this.label3.TabIndex = 10;
-            this.label3.Text = "label3";
+            this.label3.Text = "Alamat";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(100, 258);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.Size = new System.Drawing.Size(66, 20);
             this.label4.TabIndex = 11;
-            this.label4.Text = "label4";
+            this.label4.Text = "Telepon";
             // 
             // Form6
             // 
