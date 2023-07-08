6999f1575755c7dfd9630262a4d441bb3897b602
panjialbir <115150326+panjialbir@users.noreply.github.com>
2023-07-08 20:27:51 +0700

    mengganti nama tool form 3

--- UAS PABD/Form3.Designer.cs
+++ UAS PABD/Form3.Designer.cs
@@ -77,36 +77,36 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(218, 66);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(62, 20);
             this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.label1.Text = "ID Baju";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(218, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.Size = new System.Drawing.Size(80, 20);
             this.label2.TabIndex = 8;
-            this.label2.Text = "label2";
+            this.label2.Text = "Merk Baju";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(218, 153);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.Size = new System.Drawing.Size(61, 20);
             this.label3.TabIndex = 9;
-            this.label3.Text = "label3";
+            this.label3.Text = "Ukuran";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(218, 202);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.Size = new System.Drawing.Size(97, 20);
             this.label4.TabIndex = 10;
-            this.label4.Text = "label4";
+            this.label4.Text = "Harga Sewa";
             // 
             // button1
             // 
@@ -114,7 +114,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(92, 34);
             this.button1.TabIndex = 14;
-            this.button1.Text = "button1";
+            this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -123,7 +123,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 37);
             this.button2.TabIndex = 15;
-            this.button2.Text = "button2";
+            this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -132,7 +132,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(92, 26);
             this.button3.TabIndex = 16;
-            this.button3.Text = "button3";
+            this.button3.Text = "Add";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -141,7 +141,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(92, 32);
             this.button4.TabIndex = 17;
-            this.button4.Text = "button4";
+            this.button4.Text = "Open";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // comboBox1
@@ -157,9 +157,9 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(218, 245);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.Size = new System.Drawing.Size(89, 20);
             this.label5.TabIndex = 19;
-            this.label5.Text = "label5";
+            this.label5.Text = "ID Kategori";
             // 
             // Form3
             // 
