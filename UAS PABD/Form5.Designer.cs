859a7ff7420e45997779858472f28a67d43a7c27
panjialbir <115150326+panjialbir@users.noreply.github.com>
2023-07-11 23:31:32 +0700

    finish code form 5

--- UAS PABD/Form5.Designer.cs
+++ UAS PABD/Form5.Designer.cs
@@ -29,52 +29,59 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.opn = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.sve = new System.Windows.Forms.Button();
+            this.clr = new System.Windows.Forms.Button();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.idk = new System.Windows.Forms.TextBox();
+            this.nmk = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.cls = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // opn
             // 
-            this.button1.Location = new System.Drawing.Point(609, 82);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Open";
-            this.button1.UseVisualStyleBackColor = true;
+            this.opn.Location = new System.Drawing.Point(609, 82);
+            this.opn.Name = "opn";
+            this.opn.Size = new System.Drawing.Size(121, 34);
+            this.opn.TabIndex = 0;
+            this.opn.Text = "Open";
+            this.opn.UseVisualStyleBackColor = true;
+            this.opn.Click += new System.EventHandler(this.opn_Click);
             // 
-            // button2
+            // add
             // 
-            this.button2.Location = new System.Drawing.Point(609, 140);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 32);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Add";
-            this.button2.UseVisualStyleBackColor = true;
+            this.add.Location = new System.Drawing.Point(609, 140);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(119, 32);
+            this.add.TabIndex = 1;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
             // 
-            // button3
+            // sve
             // 
-            this.button3.Location = new System.Drawing.Point(609, 203);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 39);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Save";
-            this.button3.UseVisualStyleBackColor = true;
+            this.sve.Location = new System.Drawing.Point(609, 203);
+            this.sve.Name = "sve";
+            this.sve.Size = new System.Drawing.Size(121, 39);
+            this.sve.TabIndex = 2;
+            this.sve.Text = "Save";
+            this.sve.UseVisualStyleBackColor = true;
+            this.sve.Click += new System.EventHandler(this.sve_Click);
             // 
-            // button4
+            // clr
             // 
-            this.button4.Location = new System.Drawing.Point(609, 269);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(121, 37);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Clear";
-            this.button4.UseVisualStyleBackColor = true;
+            this.clr.Location = new System.Drawing.Point(609, 269);
+            this.clr.Name = "clr";
+            this.clr.Size = new System.Drawing.Size(121, 37);
+            this.clr.TabIndex = 3;
+            this.clr.Text = "Clear";
+            this.clr.UseVisualStyleBackColor = true;
+            this.clr.Click += new System.EventHandler(this.clr_Click);
             // 
             // contextMenuStrip1
             // 
@@ -82,24 +89,24 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // textBox1
+            // idk
             // 
-            this.textBox1.Location = new System.Drawing.Point(326, 90);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 26);
-            this.textBox1.TabIndex = 5;
+            this.idk.Location = new System.Drawing.Point(326, 168);
+            this.idk.Name = "idk";
+            this.idk.Size = new System.Drawing.Size(211, 26);
+            this.idk.TabIndex = 5;
             // 
-            // textBox2
+            // nmk
             // 
-            this.textBox2.Location = new System.Drawing.Point(326, 146);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(211, 26);
-            this.textBox2.TabIndex = 6;
+            this.nmk.Location = new System.Drawing.Point(326, 216);
+            this.nmk.Name = "nmk";
+            this.nmk.Size = new System.Drawing.Size(211, 26);
+            this.nmk.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(149, 93);
+            this.label1.Location = new System.Drawing.Point(165, 174);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 20);
             this.label1.TabIndex = 7;
@@ -108,27 +115,50 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(149, 152);
+            this.label2.Location = new System.Drawing.Point(165, 222);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(114, 20);
             this.label2.TabIndex = 8;
             this.label2.Text = "Nama Kategori";
+            // 
+            // cls
+            // 
+            this.cls.Location = new System.Drawing.Point(609, 327);
+            this.cls.Name = "cls";
+            this.cls.Size = new System.Drawing.Size(119, 41);
+            this.cls.TabIndex = 9;
+            this.cls.Text = "Close";
+            this.cls.UseVisualStyleBackColor = true;
+            this.cls.Click += new System.EventHandler(this.cls_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(250, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(287, 117);
+            this.dataGridView1.TabIndex = 10;
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cls);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nmk);
+            this.Controls.Add(this.idk);
+            this.Controls.Add(this.clr);
+            this.Controls.Add(this.sve);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.opn);
             this.Name = "Form5";
             this.Text = "Form5";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,14 +166,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button opn;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button sve;
+        private System.Windows.Forms.Button clr;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox idk;
+        private System.Windows.Forms.TextBox nmk;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cls;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- UAS PABD/Form5.cs
+++ UAS PABD/Form5.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,95 @@ namespace UAS_PABD
 {
     public partial class Form5 : Form
     {
+
+        private string stringConnection = "Data Source = LAPTOP-K5VL3AER;" + "database=penyewaan_baju;User ID=sa;Password=123456";
+        private SqlConnection koneksi;
+
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select nama_prodi from dbo.karyawan";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
+        private void refreshform()
+        {
+            idk.Text = "";
+            idk.Enabled = false;
+            nmk.Text = "";
+            nmk.Enabled = false;
+            sve.Enabled = false;
+            clr.Enabled = false;
+        }
         public Form5()
         {
             InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            refreshform();
+        }
+
+        private void opn_Click(object sender, EventArgs e)
+        {
+            dataGridView();
+            opn.Enabled = false;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            idk.Enabled = true;
+            nmk.Enabled = true;
+            sve.Enabled = true;
+            clr.Enabled = true;
+        }
+
+        private void sve_Click(object sender, EventArgs e)
+        {
+            string idkt = idk.Text;
+            string nmkt = nmk.Text;
+
+
+            if (nmkt == "")
+            {
+                MessageBox.Show("Masukkan Nama Kategori", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+
+
+                string query = "INSERT INTO dbo.penyewa (id_kategori, nama_kategori) VALUES (@id, @nama)";
+                using (SqlCommand command = new SqlCommand(query, koneksi))
+                {
+                    command.Parameters.Add("@id", SqlDbType.VarChar).Value = idkt;
+                    command.Parameters.Add("@nama", SqlDbType.VarChar).Value = nmkt;
+
+                    command.ExecuteNonQuery();
+                }
+
+                koneksi.Close();
+                MessageBox.Show("Data telah ditambahkan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
+            }
+        }
+        private void clr_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+
+        private void cls_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            // Membuka kembali Form1
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+
+            // Menutup aplikasi setelah menutup Form1 (jika diperlukan)
+            Application.Exit();
         }
     }
 }
